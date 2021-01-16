5aaa9dfd4486df5a8cd22ee795fe362441f3f754
Connor MacKenzie <cmm224@case.edu>
2021-01-16 02:43:19 -0500

    table schemata

---
+++ EmployeeSQL/table_shemata.sql
@@ -0,0 +1,43 @@
+CREATE TABLE Departments(
+    dept_no VARCHAR(255)
+    dept_name VARCHAR(255)
+    PRIMARY KEY (dept_no)
+);
+
+CREATE TABLE Titles(
+    title_id VARCHAR(255)
+    title VARCHAR(255)
+    PRIMARY KEY (title_id)
+);
+
+CREATE TABLE Employees(
+    emp_no INT NOT NULL
+    emp_title_id VARCHAR(255)
+    birth_date DATE
+    first_name VARCHAR(255)
+    last_name VARCHAR(255)
+    sex VARCHAR(255)
+    hire_date DATE
+    PRIMARY KEY (emp_no)
+    FOREIGN KEY (emp_title_id) REFERENCES Titles(title_id)
+);
+
+CREATE TABLE Salaries(
+    emp_no INT NOT NULL
+    salary INT NOT NULL
+    FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+);
+
+CREATE TABLE Dept_Emp(
+    emp_no INT NOT NULL
+    dept_no VARCHAR(255)
+    FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+    FOREIGN KEY (dept_no) REFERENCES Departments(dept_no)
+);
+
+CREATE TABLE Dept_Manager(
+    dept_no VARCHAR(255)
+    emp_no INT NOT NULL
+    FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+    FOREIGN KEY (dept_no) REFERENCES Departments(dept_no)
+);
