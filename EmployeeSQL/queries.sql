4e8c2b219045eed8749026126df686c61dd2a4fd
Connor MacKenzie <cmm224@case.edu>
2021-01-16 03:12:58 -0500

    first couple queries

---
+++ EmployeeSQL/queries.sql
@@ -0,0 +1,50 @@
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+
+SELECT employees.emp_no, last_name, first_name, sex, salary
+FROM employees
+JOIN salaries ON employees.emp_no = salaries.emp_no
+LIMIT 10
+
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+From employees
+where hire_date::text like '1986%'
+limit 10
+
+--List the manager of each department with the following information: 
+--department number, department name, the manager's employee number, last name, first name.
+
+Select departments.dept_no, dept_name, dept_manager.emp_no, last_name, first_name
+From departments
+join dept_manager on departments.dept_no = dept_manager.dept_no
+join employees on dept_manager.emp_no = employees.emp_no
+limit 10
+
+
+--List the department of each employee with the following information: 
+--employee number, last name, first name, and department name.
+Select employees.emp_no, last_name, first_name, dept_name
+From employees
+join dept_emp on employees.emp_no = dept_emp.emp_no
+join departments on dept_emp.dept_no = departments.dept_no
+limit 10
+
+--List first name, last name, and sex for employees 
+--whose first name is "Hercules" and last names begin with "B."
+Select first_name, last_name, sex
+From employees
+where first_name = 'Hercules'
+	and last_name like 'B%'
+
+
+--List all employees in the Sales department, 
+--including their employee number, last name, first name, and department name.
+
+
+--List all employees in the Sales and Development departments, 
+--including their employee number, last name, first name, and department name.
+
+
+--In descending order, list the frequency count of employee last names,
+--i.e., how many employees share each last name.
